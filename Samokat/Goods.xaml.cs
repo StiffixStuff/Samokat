5d14f15ce447dad98b20c681e298d4679c988e12
админ <@DESKTOP-8Q4KT2Q>
2022-04-03 01:01:36 +0300

    Добавьте файлы проекта.

---
+++ Samokat/Goods.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Samokat
+{
+    /// <summary>
+    /// Логика взаимодействия для Goods.xaml
+    /// </summary>
+    public partial class Goods : Page
+    {
+        public Goods()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void GoodsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
